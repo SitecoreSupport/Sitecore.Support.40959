18ba34c42dbe62eb9be09013895fd06c1d8a4845
DK\KIV <kirill.volovitskiy@sitecore.net>
2018-09-05 14:55:42 +0300

    Default code

---
+++ src/Sitecore.Support.40959/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.40959")]
+[assembly: AssemblyProduct("Sitecore.Support.40959")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.40959/Publishing/Service/ItemEvents/ItemOperationEmitter.cs
@@ -0,0 +1,169 @@
+﻿namespace Sitecore.Support.Publishing.Service.ItemEvents
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Reactive.Concurrency;
+  using System.Reactive.Linq;
+  using System.Reactive.Subjects;
+  using System.Threading.Tasks;
+  using Sitecore.Framework.Publishing.Locators;
+  using Sitecore.Framework.Publishing.PublisherOperations;
+  using Sitecore.Publishing.Service.ItemEvents;
+
+  public class ItemOperationEmitter : IItemOperationEmitter, IDisposable
+  {
+    private readonly IPublisherOperationService _publisherOpsService;
+
+    private readonly Subject<ItemOperationData> _itemOperationStream = new Subject<ItemOperationData>();
+
+    private readonly IScheduler _scheduler;
+    private readonly int _eventBufferingMaxCount;
+    private readonly int _eventBufferingWindowMaxMilliseconds;
+
+    private readonly TaskCompletionSource<bool> _eventStreamCompletion = new TaskCompletionSource<bool>();
+
+    public ItemOperationEmitter(
+       IPublisherOperationService publisherOpsService,
+       string eventBufferingWindowMaxMilliseconds,
+       string eventBufferingMaxCount)
+    {
+      _scheduler = Scheduler.Default;
+      _publisherOpsService = publisherOpsService;
+      _eventBufferingWindowMaxMilliseconds = int.Parse(eventBufferingWindowMaxMilliseconds);
+      _eventBufferingMaxCount = int.Parse(eventBufferingMaxCount);
+
+      Initialize();
+    }
+
+    private void Initialize()
+    {
+      _itemOperationStream
+          .ObserveOn(_scheduler)
+          .Buffer(TimeSpan.FromMilliseconds(_eventBufferingWindowMaxMilliseconds), _eventBufferingMaxCount)
+          .Where(operations => operations.Any())
+          .Subscribe(opsData =>
+          {
+                  //1) Dedupe operations
+                  var deduped = opsData
+                      .Reverse()
+                      .Distinct(new ItemOperationUriDeduperComparer())
+                      .Reverse()
+                      .ToArray();
+
+            var dedupedOps = deduped.Select(data => new PublisherOperation(
+                      0,
+                      data.VariantUri,
+                      data.Revision,
+                      new ItemPath(data.Path),
+                      data.Operation,
+                      data.Timestamp,
+                      data.Timestamp));
+
+                  //2) Expand to considers after de-duping
+                  var expandedOps = deduped
+                                      .Where(data => data.Operation != PublisherOperationType.AllVariantsDeleted)
+                                      .SelectMany(data => ExpandToConsiderOperations(data));
+
+                  //3) Persist the operations
+                  _publisherOpsService.AddPublisherOperations(dedupedOps.Concat(expandedOps).ToArray());
+          },
+          exception =>
+          {
+            _eventStreamCompletion.SetException(exception);
+          },
+          () => { _eventStreamCompletion.SetResult(true); });
+    }
+
+    private IEnumerable<PublisherOperation> ExpandToConsiderOperations(ItemOperationData data)
+    {
+      var itemPath = new ItemPath(data.Path);
+
+      if (data.Restrictions.PublishDate > DateTime.UtcNow)
+      {
+        yield return new PublisherOperation(
+            0,
+            data.VariantUri,
+            data.Revision,
+            itemPath,
+            PublisherOperationType.ConsiderItem,
+            data.Timestamp,
+            data.Restrictions.PublishDate);
+      }
+
+      if (data.Restrictions.UnpublishDate != DateTime.MaxValue.ToUniversalTime() && data.Restrictions.UnpublishDate > DateTime.UtcNow) 
+      {
+        yield return new PublisherOperation(
+            0,
+            data.VariantUri,
+            data.Revision,
+            itemPath,
+            PublisherOperationType.ConsiderItem,
+            data.Timestamp,
+            data.Restrictions.UnpublishDate);
+      }
+
+      if (data.Restrictions.ValidFrom > DateTime.UtcNow)
+      {
+        yield return new PublisherOperation(
+            0,
+            data.VariantUri,
+            data.Revision,
+            itemPath,
+            PublisherOperationType.ConsiderItem,
+            data.Timestamp,
+            data.Restrictions.ValidFrom);
+      }
+
+      if (data.Restrictions.ValidTo != DateTime.MaxValue.ToUniversalTime() && data.Restrictions.ValidTo > DateTime.UtcNow)
+      {
+        yield return new PublisherOperation(
+            0,
+            data.VariantUri,
+            data.Revision,
+            itemPath,
+            PublisherOperationType.ConsiderItem,
+            data.Timestamp,
+            data.Restrictions.ValidTo);
+      }
+    }
+
+    public void PostOperation(ItemOperationData operationData)
+    {
+      _itemOperationStream.OnNext(operationData);
+    }
+
+    public Task CloseAndFlushAsync()
+    {
+      _itemOperationStream.OnCompleted();
+      return _eventStreamCompletion.Task;
+    }
+
+    public void Dispose()
+    {
+      CloseAndFlushAsync().Wait();
+    }
+
+    private class ItemOperationUriDeduperComparer : IEqualityComparer<ItemOperationData>
+    {
+      private readonly IItemVariantLocatorComparer _uriComparer;
+
+      public ItemOperationUriDeduperComparer()
+      {
+        _uriComparer = new IItemVariantLocatorComparer();
+      }
+
+      public bool Equals(ItemOperationData x, ItemOperationData y)
+      {
+        if (x == null || y == null) return false;
+
+        return _uriComparer.Equals(x.VariantUri, y.VariantUri);
+      }
+
+      public int GetHashCode(ItemOperationData obj)
+      {
+        return _uriComparer.GetHashCode(obj.VariantUri);
+      }
+    }
+  }
+}
